aeefca0f1aa5dc974b410eb37874a092b670e0ae
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-11-24 15:30:15 +0300

    Basic dal functionalities

---
+++ backend/src/common/dal.js
@@ -0,0 +1,25 @@
+const getOne = (model) => {
+  async (props) => {
+    return await model.findOne(props);
+  };
+};
+
+const getAll = (model) => {
+  async (props) => {
+    return await model.find(props).sort({ updatedAt: -1 });
+  };
+};
+
+const createOne = (model) => {
+  async (props) => {
+    return await model.create(props);
+  };
+};
+
+const dataAccessLayer = (model) => ({
+  getOne: getOne(model),
+  getMany: getAll(model),
+  createOne: createOne(model),
+});
+
+module.exports = dataAccessLayer;
