ae6c3bb59b3c882a75ec317a8a06e920c34d4b13
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-11-29 16:59:42 +0300

    Revert "Revert "To be deleted""
    This reverts commit 24e8e52289774b974d1445cc85966ca4e04eec6e.

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+// const app = require("."),
+//   os = require("os"),
+//   dotenv = require("dotenv").config(),
+//   colors = require("colors"),
+//   logger = require("./src/common/logger"),
+//   port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   logger.info(`Running on ${os.hostname()} and listening to port ${port}`);
+// });
+
+const app = require("./src/app");
+
+module.exports = app;
