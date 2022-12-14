d063971e7f32698e73ebad5770be172a4fb8da1f
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-12-14 16:55:48 +0300

    Added delete and update funtions

--- backend/src/common/dal.js
+++ backend/src/common/dal.js
@@ -1,3 +1,5 @@
+const { model } = require("mongoose");
+
 const getOne = (model) => {
   return async (props) => {
     return await model.findOne(props);
@@ -12,10 +14,20 @@ const createOne = (model) => async (props) => {
   return await model.create(props);
 };
 
+const updateOne = (model) => async (filter, props) => {
+  return await model.findOneAndUpdate(filter, {
+    $set: props,
+  });
+};
+const deleteOne = (model) => async (filter) => {
+  return await model.findAndDelete(filter);
+};
 const dataAccessLayer = (model) => ({
   getOne: getOne(model),
   getMany: getAll(model),
   createOne: createOne(model),
+  updateOne: updateOne(model),
+  deleteOne: deleteOne(model),
 });
 
 module.exports = dataAccessLayer;
